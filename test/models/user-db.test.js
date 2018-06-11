c712ebd5ec583a0e186d30cf0fca748a893b91f7
Aleksandr I <vigntom@gmail.com>
2018-06-11 22:07:28 +0300

    Fix tests

--- test/models/user-db.test.js
+++ test/models/user-db.test.js
@@ -11,7 +11,7 @@ test.after.always(() => {
   return User.remove()
 })
 
-test.only.cb('username should be unique', t => {
+test.cb('username should be unique', t => {
   const signup = db.fixture.user(login)
   const user = new User(signup)
 
